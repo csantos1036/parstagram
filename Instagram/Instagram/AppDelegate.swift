abb1cadcc2c70c2e04d7e8d54b141d812db9a7d8
csantos1036 <santos10carolina@gmail.com>
2021-10-18 23:30:31 -0400

    Updated capabilities to logout and post comments

--- Instagram/Instagram/AppDelegate.swift
+++ Instagram/Instagram/AppDelegate.swift
@@ -15,12 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
         let parseConfig = ParseClientConfiguration {
                     $0.applicationId = "Xr9zXE9CIrAnzXxiu2fICDjAlutVygRc2bu8ltBO"
                     $0.clientKey = "bMk6VzlO7Khdtz1jXbaoldNCVRYDBClhznZ2Za8f"
                     $0.server = "https://parseapi.back4app.com"
             }
             Parse.initialize(with: parseConfig)
+            
         return true
     }
 
